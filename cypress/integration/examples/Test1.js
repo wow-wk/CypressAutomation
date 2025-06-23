ef5369a123de1c4b694c5afa3cc69c9d7d18fa78
wow-wk <werasut.k@gmail.com>
2025-06-23 17:50:42 +0700

    Add refer to cypress and try test case 1

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -1,5 +1,12 @@
+/// <reference types='Cypress'/>
+
 describe('My First Test Suite', function() {
     it('My First Test Case', function () {
-        cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/');
+        cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/')
+        cy.get('.search-keyword').type('ca')
+        cy.wait(2000)
+        
+        // Selenium get hit url in browser, cypress get acts like findElement of selenium
+        cy.get('.product:visible').should('have.length', 4)
     })
 })
