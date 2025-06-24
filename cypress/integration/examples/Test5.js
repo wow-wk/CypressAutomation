3dc6a9f9cd9b881edf6ac278aaefe94fbfcbc03e
wow-wk <werasut.k@gmail.com>
2025-06-24 15:03:09 +0700

    Handling child tab on test runner

---
+++ cypress/integration/examples/Test5.js
@@ -0,0 +1,16 @@
+/// <reference types='Cypress'/>
+
+describe('Handling Child Windows', function() {
+    it('Should handle child window', function () {
+        cy.visit('https://rahulshettyacademy.com/AutomationPractice/')
+        
+        // Remove open new tab
+        cy.get('#opentab').invoke('removeAttr', 'target').click()
+    
+        // Change origin to another domain
+        cy.origin('https://www.qaclickacademy.com', () => {
+            cy.get('#navbarSupportedContent a[href*="about"]').click()
+            cy.get('.mt-50 h2').should('contain', 'QAClick Academy')
+        })
+    })
+})
