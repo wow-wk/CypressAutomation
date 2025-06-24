fe5adc428f1896fdeac24512aab1ec5fc411a6af
wow-wk <werasut.k@gmail.com>
2025-06-24 13:28:09 +0700

    Handling static / dynamic dropdown types

--- cypress/integration/examples/Test3.js
+++ cypress/integration/examples/Test3.js
@@ -3,8 +3,22 @@
 describe('My Third Test Suite', function() {
     it('My First Test Case', function () {
         cy.visit('https://rahulshettyacademy.com/AutomationPractice/')
+        
+        // Check Boxes
         cy.get('#checkBoxOption1').check().should('be.checked').and('have.value', 'option1')
         cy.get('#checkBoxOption1').uncheck().should('not.be.checked')
         cy.get('input[type="checkbox"]').check(['option2', 'option3'])
+    
+        // Static Dropdown
+        cy.get('select').select('option2').should('have.value', 'option2')
+
+        // Dynamic Dropdown
+        cy.get('#autocomplete').type('ind')
+        cy.get('.ui-menu-item div').each(($el, index, $list) => {
+            if($el.text() === 'India') {
+                cy.wrap($el).click()
+            }
+        })
+        cy.get('#autocomplete').should('have.value', 'India')
     })
 })
