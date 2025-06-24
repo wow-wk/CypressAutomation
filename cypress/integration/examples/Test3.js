08e3a445c4c5f9cbc8e65e4b41db92e9d63ac417
wow-wk <werasut.k@gmail.com>
2025-06-24 13:15:38 +0700

    Add third test for try any input type

---
+++ cypress/integration/examples/Test3.js
@@ -0,0 +1,10 @@
+/// <reference types='Cypress'/>
+
+describe('My Third Test Suite', function() {
+    it('My First Test Case', function () {
+        cy.visit('https://rahulshettyacademy.com/AutomationPractice/')
+        cy.get('#checkBoxOption1').check().should('be.checked').and('have.value', 'option1')
+        cy.get('#checkBoxOption1').uncheck().should('not.be.checked')
+        cy.get('input[type="checkbox"]').check(['option2', 'option3'])
+    })
+})
